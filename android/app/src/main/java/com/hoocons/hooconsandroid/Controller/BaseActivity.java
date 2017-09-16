5a91e5c55cd8b51a258a778bb36c218e300e641c
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-09-16 11:11:20 -0400

    added needed dependencies

---
+++ android/app/src/main/java/com/hoocons/hooconsandroid/Controller/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.hoocons.hooconsandroid.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hungnguyen on 9/16/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+
+}
