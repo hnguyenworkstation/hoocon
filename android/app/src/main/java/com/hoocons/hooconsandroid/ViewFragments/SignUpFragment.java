8317a81620c40f034d895e2fb6840653517b8ce4
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-09-17 10:29:04 -0400

    set detault text and typeface for signup fragment

--- android/app/src/main/java/com/hoocons/hooconsandroid/ViewFragments/SignUpFragment.java
+++ android/app/src/main/java/com/hoocons/hooconsandroid/ViewFragments/SignUpFragment.java
@@ -2,22 +2,53 @@ package com.hoocons.hooconsandroid.ViewFragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hoocons.hooconsandroid.R;
+import com.klinker.android.link_builder.Link;
+import com.klinker.android.link_builder.LinkBuilder;
+import com.vstechlab.easyfonts.EasyFonts;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class SignUpFragment extends Fragment {
+    @BindView(R.id.signup_title)
+    TextView mTitle;
+    @BindView(R.id.username)
+    TextView mUserName;
+    @BindView(R.id.password)
+    EditText mPassword;
+    @BindView(R.id.re_password)
+    EditText mRePassword;
+    @BindView(R.id.signup)
+    Button mSignUpBtn;
+    @BindView(R.id.condition)
+    TextView mCondition;
+
+    private final static String USERNAME_ARGS = "USERNAME";
+    private String userName;
+    private Unbinder unbinder;
 
     public SignUpFragment() {
         // Required empty public constructor
     }
 
-    public static SignUpFragment newInstance() {
+    public static SignUpFragment newInstance(String userName) {
         SignUpFragment fragment = new SignUpFragment();
         Bundle args = new Bundle();
+
+        args.putString(USERNAME_ARGS, userName);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -26,14 +57,63 @@ public class SignUpFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            userName = getArguments().getString(USERNAME_ARGS);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+
+        initDefaultTextAndTypeFace();
+    }
+
+    private void initDefaultTextAndTypeFace() {
+        mTitle.setText(getResources().getText(R.string.signup));
+        mPassword.setHint(getResources().getText(R.string.password));
+        mRePassword.setHint(getResources().getText(R.string.confirm_password));
+        mUserName.setText(userName);
+        mSignUpBtn.setText(getResources().getText(R.string.signup));
+        mCondition.setText(getResources().getText(R.string.clicked_signup_condition));
+        catchConditionLink(mCondition);
+
+        mTitle.setTypeface(EasyFonts.robotoBold(getContext()));
+        mPassword.setTypeface(EasyFonts.robotoRegular(getContext()));
+        mRePassword.setTypeface(EasyFonts.robotoRegular(getContext()));
+        mUserName.setTypeface(EasyFonts.robotoRegular(getContext()));
+        mSignUpBtn.setTypeface(EasyFonts.robotoRegular(getContext()));
+        mCondition.setTypeface(EasyFonts.robotoRegular(getContext()));
+    }
+
+    private void catchConditionLink(TextView textView) {
+        final Link link = new Link(getResources().getString(R.string.term_and_condition));
+        link.setTextColor(R.color.colorPrimary);
+        link.setTypeface(EasyFonts.robotoRegular(getContext()));
+        link.setBold(true);
+        link.setUnderlined(false);
+
+        link.setOnClickListener(new Link.OnClickListener() {
+            @Override
+            public void onClick(String clickedText) {
+                Toast.makeText(getContext(), "Clicked" + link.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        LinkBuilder.on(textView)
+                .addLink(link)
+                .build();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
