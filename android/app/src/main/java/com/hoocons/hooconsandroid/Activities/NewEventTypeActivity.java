239c6ff919557a0103cbfd9203d7bce56906adb0
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-10-02 21:24:14 -0400

    fixed background runnable between activities

--- android/app/src/main/java/com/hoocons/hooconsandroid/Activities/NewEventTypeActivity.java
+++ android/app/src/main/java/com/hoocons/hooconsandroid/Activities/NewEventTypeActivity.java
@@ -3,14 +3,17 @@ package com.hoocons.hooconsandroid.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.ppamorim.dragger.DraggerActivity;
 import com.hoocons.hooconsandroid.AppController.BaseActivity;
 import com.hoocons.hooconsandroid.R;
 
-public class NewEventTypeActivity extends BaseActivity {
+public class NewEventTypeActivity extends DraggerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event_type);
+
+        setDraggerLimit(0.75f);
     }
 }
--- android/app/src/main/java/com/hoocons/hooconsandroid/Helpers/AppUtils.java
+++ android/app/src/main/java/com/hoocons/hooconsandroid/Helpers/AppUtils.java
@@ -3,6 +3,7 @@ package com.hoocons.hooconsandroid.Helpers;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -22,13 +23,43 @@ public class AppUtils {
         return googlePlayServicesAvailable == ConnectionResult.SUCCESS;
     }
 
-    public static void startNewActivity(@NonNull Context context, @Nullable Activity fromActivity,
-                                        @NonNull Class toActivity) {
-        context.startActivity(new Intent(fromActivity, toActivity));
+    public static void startNewActivity(@NonNull final Context context, @Nullable final Activity fromActivity,
+                                        @NonNull final Class toActivity) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                context.startActivity(new Intent(fromActivity, toActivity));
+            }
+        };
+        Handler handler = new Handler();
+        handler.postDelayed(runnable, 1000);
     }
 
-    public static void loadWebsite(@NonNull Context context, @NonNull Activity fromActivity, String url) {
-        context.startActivity(new Intent(fromActivity, WebPageActivity.class)
-                .putExtra(WebPageActivity.WEB_URL, url));
+    public static void startNewActivityAndFinish(@NonNull final Context context,
+                                                 @NonNull final Activity fromActivity,
+                                                 @NonNull final Class toActivity) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                context.startActivity(new Intent(fromActivity, toActivity));
+                fromActivity.finish();
+            }
+        };
+        Handler handler = new Handler();
+        handler.postDelayed(runnable, 1000);
+    }
+
+    public static void loadWebsite(@NonNull final Context context,
+                                   @NonNull final Activity fromActivity,
+                                   final String url) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                context.startActivity(new Intent(fromActivity, WebPageActivity.class)
+                        .putExtra(WebPageActivity.WEB_URL, url));
+            }
+        };
+        Handler handler = new Handler();
+        handler.postDelayed(runnable, 1000);
     }
 }
